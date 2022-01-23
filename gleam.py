6c635427da4f3d6fd52fdeca983d881e15f9f829
Ashton <ash09er5@gmail.com>
2022-01-23 16:27:58 -0500

    Add files via upload

---
+++ gleam.py
@@ -0,0 +1,28 @@
+from wtforms import fields
+from ggplot import *
+
+from gleam import Page, panels
+
+class ScatterInput(panels.Inputs):
+    title = fields.StringField(label="Title of plot:")
+    yvar = fields.SelectField(label="Y axis",
+                              choices=[("beef", "Beef"),
+                                       ("pork", "Pork")])
+    smoother = fields.BooleanField(label="Smoothing Curve")
+
+
+class ScatterPlot(panels.Plot):
+    name = "Scatter"
+
+    def plot(self, inputs):
+        p = ggplot(meat, aes(x='date', y=inputs.yvar))
+        if inputs.smoother:
+            p = p + stat_smooth(color="blue")
+        p = p + geom_point() + ggtitle(inputs.title)
+        return p
+
+class ScatterPage(Page):
+    input = ScatterInput()
+    output = ScatterPlot()
+
+ScatterPage.run()
